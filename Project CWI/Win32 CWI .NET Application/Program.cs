3fadc43edde2fb27c06b0d1def0b35c8013249b8
Ryan Walpole <ryan.walpole@outlook.com>
2022-11-25 15:21:34 +1100

    Create Program.cs
    Add starting code for the program
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ Project CWI/Win32 CWI .NET Application/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win32_CWI.NET_Application
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string AppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string LabsAppData = AppData + @"\RWE Labs\DKC-Toolbox\";
+            string VersionFile = LabsAppData + @"version.txt";
+
+            if (!Directory.Exists(LabsAppData))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Setup());
+            }
+            else
+            {
+                if(Properties.Settings.Default.PythonVersion == "RESET")
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Setup());
+                }
+                else
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Splash());
+                }
+            }
+            
+        }
+    }
+}
