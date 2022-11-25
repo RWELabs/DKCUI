ee75e3058ab9cc595c9edf56a3122eac0c0b1a3d
Ryan Walpole <ryan.walpole@outlook.com>
2022-11-25 15:19:47 +1100

    Add Window (Update)
    Add update window for installing the latest DKCUI updates.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ Project CWI/Win32 CWI .NET Application/UIUpdateDownload.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Win32_CWI.NET_Application
+{
+    partial class UIUpdateDownload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UIUpdateDownload));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StatusText = new System.Windows.Forms.Label();
+            this.InstallProgress = new System.Windows.Forms.ProgressBar();
+            this.StartExecute = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Win32_CWI.NET_Application.Properties.Resources.Loading;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "DKCUI Update";
+            // 
+            // StatusText
+            // 
+            this.StatusText.AutoSize = true;
+            this.StatusText.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.StatusText.Location = new System.Drawing.Point(118, 53);
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(73, 13);
+            this.StatusText.TabIndex = 6;
+            this.StatusText.Text = "Please wait...";
+            // 
+            // InstallProgress
+            // 
+            this.InstallProgress.Location = new System.Drawing.Point(121, 75);
+            this.InstallProgress.Name = "InstallProgress";
+            this.InstallProgress.Size = new System.Drawing.Size(247, 23);
+            this.InstallProgress.TabIndex = 8;
+            // 
+            // StartExecute
+            // 
+            this.StartExecute.Interval = 2500;
+            this.StartExecute.Tick += new System.EventHandler(this.StartExecute_Tick);
+            // 
+            // Download
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(400, 132);
+            this.ControlBox = false;
+            this.Controls.Add(this.InstallProgress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StatusText);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Download";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label StatusText;
+        private System.Windows.Forms.ProgressBar InstallProgress;
+        private System.Windows.Forms.Timer StartExecute;
+    }
+}
